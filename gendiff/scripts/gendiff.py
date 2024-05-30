fe175bbeb7dd920de99f34ae8edbe9cea169a1e5
melloru <pythonubuntu12@gmail.com>
2024-05-30 17:27:00 +0300

    #2 step | add format to gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,8 +6,9 @@ def main():
         description="Compares two configuration "
                     "files and shows a difference."
     )
-    parser.add_argument("first_file")
-    parser.add_argument("second_file")
+    parser.add_argument("first_file", type=str)
+    parser.add_argument("second_file", type=str)
+    parser.add_argument("-f", '--format', help='set format of output', type=str)
 
     args = parser.parse_args()
 
