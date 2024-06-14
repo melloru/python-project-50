e4224eaf22c7c5455e90e043d51db2aab31048c4
melloru <pythonubuntu12@gmail.com>
2024-06-14 15:42:33 +0300

    #5 step | add YAML format

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,16 +1,20 @@
 import json
+import yaml
 
 
 def is_bool(value):
     if isinstance(value, bool):
         return str(value).lower()
     return value
-
+def open_file(file):
+    if file.endswith('json'):
+        return json.load(open(file))
+    return yaml.load(open(file), Loader=yaml.FullLoader)
 
 def generate_diff(file_path1, file_path2):
     diff = []
-    file1 = json.load(open(file_path1))
-    file2 = json.load(open(file_path2))
+    file1 = open_file(file_path1)
+    file2 = open_file(file_path2)
     for key in sorted(set(file1.keys() | file2.keys())):
 
         if key not in file1:
--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -9,3 +9,13 @@ import pytest
 ])
 def test_json(file1, file2, result):
     assert generate_diff(file1, file2) == open(result).read()
+
+
+@pytest.mark.parametrize('file1, file2, result', [
+    ('tests/fixtures/yaml/file1.yaml',
+     'tests/fixtures/yaml/file2.yaml',
+     'tests/fixtures/yaml/result_yaml')
+])
+def test_json(file1, file2, result):
+    assert generate_diff(file1, file2) == open(result).read()
+
